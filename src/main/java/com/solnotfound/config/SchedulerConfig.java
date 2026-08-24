97a83ff04615b6d5a5ae1f8875b82c022c5e3b63
nicopiacentini <npiacentini@frba.utn.edu.ar>
2026-08-24 16:13:06 -0300

    fix: faltaba hacer bean a la funcion del config para que sea detectado por spring

--- src/main/java/com/solnotfound/config/SchedulerConfig.java
+++ src/main/java/com/solnotfound/config/SchedulerConfig.java
@@ -1,10 +1,12 @@
 package com.solnotfound.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.scheduling.concurrent.ThreadPoolTaskScheduler;
 
 @Configuration
 public class SchedulerConfig {
+  @Bean
   public ThreadPoolTaskScheduler taskScheduler() {
     ThreadPoolTaskScheduler scheduler = new ThreadPoolTaskScheduler();
     scheduler.setPoolSize(5);
