a9883e90a4b106be0c541b1e12eb8a27815def3f
fpastorino <fpastorino@frba.utn.edu.ar>
2026-08-29 00:28:19 -0300

    modifico ActivityRepository y agrego VotationRepository jusnto con sus mocks

--- src/main/java/com/solnotfound/repository/ActivityRepository.java
+++ src/main/java/com/solnotfound/repository/ActivityRepository.java
@@ -2,38 +2,15 @@ package com.solnotfound.repository;
 
 import com.solnotfound.entity.Activity;
 import java.util.List;
-import java.util.Map;
-import java.util.concurrent.ConcurrentHashMap;
-import org.springframework.stereotype.Repository;
 
-@Repository
-public class ActivityRepository { // TODO: Por ahora se guardan en memoria
-  private final Map<String, Activity> activities = new ConcurrentHashMap<>();
+public interface ActivityRepository {
+  public void save(Activity activity);
 
-  public void save(Activity activity) {
-    activities.put(activity.getId(), activity);
-  }
+  public List<Activity> findAll();
 
-  public List<Activity> findAll() {
-    return List.copyOf(activities.values());
-  }
+  public Activity findById(String id);
 
-  public Activity findById(String id) {
-    return activities.get(id);
-  }
+  public List<Activity> findActivitiesByOrganizerId(String organizerId);
 
-  public List<Activity> findActivitiesByOrganizerId(String organizerId) {
-    return activities.values().stream()
-      .filter(activity -> activity.getOrganizer().getId().equals(organizerId))
-      .toList();
-  }
-
-  public List<Activity> findActivitiesByParticipantId(String participantId) {
-    return activities.values().stream()
-      .filter(activity ->
-        activity.getParticipants().stream()
-          .anyMatch(participant -> participant.getId().equals(participantId)))
-      .toList();
-  }
-
+  public List<Activity> findActivitiesByParticipantId(String participantId);
 }
