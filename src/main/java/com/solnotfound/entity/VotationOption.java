fad61c931fb2afb90c3132e4d49b5c6cd6e2c027
fpastorino <fpastorino@frba.utn.edu.ar>
2026-08-28 21:10:59 -0300

    agrego las necesarias para modelar las votaciones

---
+++ src/main/java/com/solnotfound/entity/VotationOption.java
@@ -0,0 +1,11 @@
+package com.solnotfound.entity;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+public class VotationOption {
+
+  private LocalDateTime dateTime;
+  private List<User> users;
+
+}
