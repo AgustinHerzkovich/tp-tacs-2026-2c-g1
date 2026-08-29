17c23964707f7c2a2c10bdb001663b549a3ff055
nicopiacentini <npiacentini@frba.utn.edu.ar>
2026-08-29 19:25:14 -0300

    feature: estadisticas de actividades

--- src/main/java/com/solnotfound/exception/GlobalExceptionHandler.java
+++ src/main/java/com/solnotfound/exception/GlobalExceptionHandler.java
@@ -96,6 +96,31 @@ public class GlobalExceptionHandler {
     problem.setProperty("parameter", exception.getName());
     return ResponseEntity.badRequest().body(problem);
   }
+  @ExceptionHandler(CouldNotRetrieveStatisticsException.class)
+  public ProblemDetail handleStatisticsServiceUnavailable(CouldNotRetrieveStatisticsException ex) {
+    ProblemDetail problemDetail = ProblemDetail.forStatusAndDetail(
+      HttpStatus.SERVICE_UNAVAILABLE,
+      ex.getMessage()
+    );
+    problemDetail.setTitle("Servicio No Disponible");
+
+    // Es buena práctica indicarle al cliente si vale la pena reintentar
+    problemDetail.setProperty("retryable", true);
+    problemDetail.setProperty("timestamp", Instant.now());
+
+    return problemDetail;
+  }
+  @ExceptionHandler(InvaildActivityStatusException.class)
+    public ProblemDetail handleInvalidActivityStatus(InvaildActivityStatusException ex) {
+        ProblemDetail problemDetail = ProblemDetail.forStatusAndDetail(
+        HttpStatus.BAD_REQUEST,
+        ex.getMessage()
+        );
+        problemDetail.setTitle("Invalid Activity Status");
+        problemDetail.setProperty("timestamp", Instant.now());
+
+        return problemDetail;
+    }
 
   @ExceptionHandler(CouldNotRetrieveStatisticsException.class)
   public ProblemDetail handleStatisticsServiceUnavailable(CouldNotRetrieveStatisticsException ex) {
