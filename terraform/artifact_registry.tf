19eef2810c667d921cac3f8ef9708973273f59df
AgustinHerzkovich <aherzkovich@frba.utn.edu.ar>
2026-09-21 21:05:40 -0300

    fix: corrijo pipelines

--- terraform/artifact_registry.tf
+++ terraform/artifact_registry.tf
@@ -7,3 +7,17 @@ resource "google_artifact_registry_repository" "containers" {
 
   depends_on = [google_project_service.required]
 }
+
+resource "google_artifact_registry_repository_iam_member" "runtime_readers" {
+  for_each = {
+    backend  = google_service_account.backend.email
+    frontend = google_service_account.frontend.email
+    keycloak = google_service_account.keycloak.email
+  }
+
+  project    = google_artifact_registry_repository.containers.project
+  location   = google_artifact_registry_repository.containers.location
+  repository = google_artifact_registry_repository.containers.repository_id
+  role       = "roles/artifactregistry.reader"
+  member     = "serviceAccount:${each.value}"
+}
